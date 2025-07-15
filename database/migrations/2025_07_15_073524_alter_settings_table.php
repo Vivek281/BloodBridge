75ce4bf537da71b7c6ebe314bbf1dd361341799f
Vivek281 <vivekjha2062@gmail.com>
2025-07-15 18:51:24 +0545

    Backend was modified, frontend was added, site setting was done, site setting errrors were resolved

---
+++ database/migrations/2025_07_15_073524_alter_settings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('Settings', function (Blueprint $table) {
+            $table->string('key')->nullable();
+            $table->string('value')->nullable();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('Settings', function (Blueprint $table) {
+            //
+        });
+    }
+}
